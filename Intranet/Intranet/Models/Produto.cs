8cfd3a2c90fbf6dfea49c1075f88425a6babf13f
Ivo Guilherme <ivo.guiilherme@hotmail.com>
2018-06-02 19:53:05 -0300

    Implementação da classe Produto; Criação da view do Create;

---
+++ Intranet/Intranet/Models/Produto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace Intranet.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Descrição do Produto")]
+        public string DescProduto { get; set; }
+
+        [DisplayName("Peso")]
+        public float Peso { get; set; }
+
+        [Required]
+        [DisplayName("Valor")]
+        public float Valor { get; set; }
+
+        [DisplayName("Quantidade")]
+        public int Quantidade { get; set; }
+
+        [DisplayName("Data de Compra")]
+        public DateTime DataCompra { get; set; }
+
+        [DisplayName("Data de Vencimento")]
+        public DateTime DataVencimento { get; set; }
+
+        [DisplayName("Ativo")]
+        public bool isAtivo { get; set; }
+
+    }
+}
